b6f7d8caf221d6c476218a264079a27a625f5806
Mehmet Emin Tellioğlu <mehmetemintellioglu@Mehmet-Mac.local>
2025-01-30 20:36:56 +0300

    hatasiz calisiyor da tarayicida gozukmuyor

--- frontend/src/reportWebVitals.ts
+++ frontend/src/reportWebVitals.ts
@@ -1,12 +1,12 @@
-import { Metric, onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
+import { getCLS, getFID, getFCP, getLCP, getTTFB, Metric } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: (metric: Metric) => void): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    onCLS(onPerfEntry);
-    onFID(onPerfEntry);
-    onFCP(onPerfEntry);
-    onLCP(onPerfEntry);
-    onTTFB(onPerfEntry);
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
